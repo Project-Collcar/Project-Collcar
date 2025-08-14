b8e29b43e6a8af6a1e9b9a7b3676856a12f8ede4
JO SUBIN <86547611+theFireFly-Night@users.noreply.github.com>
2025-08-15 01:22:10 +0900

    Add: Speeder vehicles and individual gravity implementation
    Speeder(신속형) 차량에 대한 스크립트 구현 및 개별 중력 구현 시스템 중력은 존재하나 차량의 rigidbody의 중력 설정을 off로 설정. 시스템 중력 0으로 바꾸면 WheelCollider가 오류로 수직마찰력 작용하지 못함으로 개별 설정 중력 off로 바꿈.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -9,30 +9,26 @@ public class Car : MonoBehaviour
     public Wheel rearLeft;
     public Wheel rearRight;
 
-    [Header("Car Specs")] // 차량 능력치
-    public float motorTorque = 0f;
-    public float maxMotorTorque = 0f;
-    public float steerAngle = 0f;
-    public float brakeTorque = 0f;
-    
     [Header("Physics")] // 물리 기능
     public Transform centerOfMassObject;
-    private Rigidbody rigidBody;
-
+    protected Rigidbody rigidBody;
     
-    /*
-    [Header("Effects & Sounds")]  // 효과 및 사운드
-    public GameObject[] explosionEffect;
-    public AudioClip engineSound;
-    */
+    // 자식 클래스에서 덮어쓸 능력치 변수들 
+    protected float motorTorque;
+    protected float steerAngle;
+    protected float brakeTorque;
+    protected float gravityMultiplier = 1.0f;
 
-    private void Start()
+    // 자식이 덮어쓸 수 있도록 protected virtual로 선언
+    protected virtual void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
-        rigidBody.centerOfMass = centerOfMassObject.localPosition;
+        if (centerOfMassObject != null)
+            rigidBody.centerOfMass = centerOfMassObject.localPosition;
     }
 
-    private void FixedUpdate()
+    // 모든 차량이 공통으로 사용할 운전 로직
+    protected virtual void FixedUpdate()
     {
         // 1. 사용자 입력
         float steerInput = Input.GetAxis("Horizontal");
@@ -43,23 +39,33 @@ public class Car : MonoBehaviour
         float steer = steerInput * steerAngle;
         float motor = throttleInput * motorTorque;
         float brake = 0f;
-        // 핸드브레이크 로직
         if (handBrake)
             brake = brakeTorque;
 
         // 3. 계산된 값 적용
-        // 앞바퀴 조향
         frontLeft.SetSteerAngle(steer);
         frontRight.SetSteerAngle(steer);
         
-        // 뒷바퀴 구동
         rearLeft.SetMotorTorque(motor);
         rearRight.SetMotorTorque(motor);
         
-        // 모든 바퀴 브레이크
         frontLeft.SetBrakeTorque(brake);
         frontRight.SetBrakeTorque(brake);
         rearLeft.SetBrakeTorque(brake);
         rearRight.SetBrakeTorque(brake);
+        
+        // 4. 커스텀 중력
+        rigidBody.AddForce(Vector3.down * (9.81f * gravityMultiplier), ForceMode.Acceleration);
+    }
+
+    // 자식들이 각자의 스킬을 구현할 수 있는 틀
+    public virtual void UseSkill1()
+    {
+        Debug.Log("이 차량은 1번 스킬이 없습니다.");
+    }
+
+    public virtual void UseSkill2()
+    {
+        Debug.Log("이 차량은 2번 스킬이 없습니다.");
     }
 }
